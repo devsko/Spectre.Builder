5a0e28fed8af1340096bac9e6c87023697b8b91c
devsko <devsko@outlook.com>
2025-06-07 12:42:50 +0200

    add a simple Progress class

---
+++ src/Spectre.Builder/Progress.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) devsko. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Spectre.Builder;
+
+/// <summary>
+/// Represents a progress reporter that invokes a specified handler when progress is reported.
+/// </summary>
+/// <typeparam name="T">
+/// The type of progress update value.
+/// </typeparam>
+public class Progress<T>(Action<T> handler) : IProgress<T>
+{
+    /// <inheritdoc/>
+    public void Report(T value)
+    {
+        handler(value);
+    }
+}
